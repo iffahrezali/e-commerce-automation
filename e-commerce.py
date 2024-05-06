18cd57025577d1d3c3e44961c4a1c66840e5acdb
Iffah R <92578072+iffahrezali@users.noreply.github.com>
2024-05-06 21:40:10 +0800

    Create functions for test steps

--- e-commerce.py
+++ e-commerce.py
@@ -1,5 +1,4 @@
 import time
-import travel_info
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
@@ -14,6 +13,24 @@ wait = WebDriverWait(driver, 30)
 def openBrowser(input_link):
     driver.get(input_link)
     driver.maximize_window()
+
+def loginWebsite(username, password):
+
+#Verify the website loads to the homepage
+def homePage():
+    
+#Add items into shopping cart
+def addItems():
+
+#Verify numbers of items added to cart matched
+def verifyItems():
+
+#Check out the items from shopping cart
+def checkoutItems():
+
+#Proceed for payment
+def payItems():
+    
 
 if __name__ == "__main__":
     input_link = 'https://www.saucedemo.com'
